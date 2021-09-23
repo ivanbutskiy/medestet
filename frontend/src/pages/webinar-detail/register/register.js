82bb3122639bb501e19653324ce533aa8136a3df
ivan_butskiy <butskiy1795@gmail.com>
2021-09-23 21:49:23 +0300

    Stop redirect to WFP after register for the webinar

--- frontend/src/pages/webinar-detail/register/register.js
+++ frontend/src/pages/webinar-detail/register/register.js
@@ -162,11 +162,12 @@ class Register extends Component {
 
         this.service.webinarOrderRegister(orderReference, webinarId, selectedOptionId, promocode)
             .then(result => {
-                if (selectedOptionPrice > 0) {
-                    f.submit();
-                } else {
-                    this.setState({ successRedirect: true })
-                }
+                this.setState({ successRedirect: true })
+                // if (selectedOptionPrice > 0) {
+                //     f.submit();
+                // } else {
+                //     this.setState({ successRedirect: true })
+                // }
             }).catch(error => {
                 this.setState({ error: true })
             });
@@ -406,7 +407,8 @@ class Register extends Component {
 
                 </div>
                 
-                <small>Общая сумма ваших покупок составляет { buySum } грн. Обратите внимание, что при достижении общей суммы покупок в 5000 грн каждому зарегистрированному пользователю становится доступной скидка 3%, а при достижении 18000 грн – 5%. При выборе платного варианта вас переведет на платежный шлюз <strong>WayForPay</strong>, где вы сможете оплатить участие.</small>
+                <small>Общая сумма ваших покупок составляет { buySum } грн. Обратите внимание, что при достижении общей суммы покупок в 5000 грн каждому зарегистрированному пользователю становится доступной скидка 3%, а при достижении 18000 грн – 5%.</small>
+                // <small>Общая сумма ваших покупок составляет { buySum } грн. Обратите внимание, что при достижении общей суммы покупок в 5000 грн каждому зарегистрированному пользователю становится доступной скидка 3%, а при достижении 18000 грн – 5%. При выборе платного варианта вас переведет на платежный шлюз <strong>WayForPay</strong>, где вы сможете оплатить участие.</small>
             </div>
         );
     };
